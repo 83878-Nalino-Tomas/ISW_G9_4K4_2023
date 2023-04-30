e0c922954bbef4e6fc0a7c8885a7ec22b1006a3e
EmmaGalizio <emmagalzio@gmail.com>
2023-04-30 20:30:09 -0300

    Documentación TP6

--- Practico/TP_Evaluables/Trabajo_Practico_N6/Delivery_Loquesea/app/build/generated/source/buildConfig/debug/isw/grupo9/BuildConfig.java
+++ Practico/TP_Evaluables/Trabajo_Practico_N6/Delivery_Loquesea/app/build/generated/source/buildConfig/debug/isw/grupo9/BuildConfig.java
@@ -4,7 +4,7 @@
 package isw.grupo9;
 
 public final class BuildConfig {
-  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "isw.grupo1";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
